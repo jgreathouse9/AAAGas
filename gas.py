1fabe860804fe86fdb0651235a4faefbe1338660
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2024-11-16 09:39:41 -0500

    Update gas.py

--- gas.py
+++ gas.py
@@ -1,5 +1,6 @@
 from scraper import scrape_gas_prices
 from datetime import datetime
+import os
 
 # Example usage
 url = "https://gasprices.aaa.com/state-gas-price-averages/"
@@ -10,9 +11,11 @@ gas_prices_df = scrape_gas_prices(url, css_selector)
 today_date = datetime.now().strftime('%Y-%m-%d')  # Format: YYYY-MM-DD
 gas_prices_df['Date'] = today_date
 
-# Save the DataFrame to CSV
-output_file = f"gas_prices_{today_date}.csv"
-gas_prices_df.to_csv(output_file, index=False)
+# Create 'Prices' folder if it doesn't exist
+output_dir = "Prices"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
-# Display the DataFrame for debugging
-print(gas_prices_df)
+# Save the DataFrame to a CSV file in the 'Prices' folder
+output_file = os.path.join(output_dir, f"gas_prices_{today_date}.csv")
+gas_prices_df.to_csv(output_file, index=False)
