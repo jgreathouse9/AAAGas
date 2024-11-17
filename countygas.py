994cfc4750d135fcd9a40bc0907d956605daf6a9
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2024-11-17 15:00:22 -0500

    Update countygas.py

--- countygas.py
+++ countygas.py
@@ -1,21 +1,19 @@
 from countyscraper import get_all_state_data, plot_city_gas_prices
 import os
 
-# Get all metro-level gas price data
+# Get all county-level gas price data (already sorted)
 master_df = get_all_state_data()
 
-# Creating the relevant directory
+# Ensure the 'CountyPrices' directory exists
 output_dir = "CountyPrices"
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-# Dropping duplicates
-master_df = master_df.drop_duplicates(subset=['State', 'City', 'Date'])
+# Save the updated master DataFrame
 master_file = os.path.join(output_dir, "MasterGas.csv")
-
-#!! Saving the combined df sans duplicates
 master_df.to_csv(master_file, index=False)
 
-# Generate plot for ATL
-plot_path = os.path.join(output_dir, "GasPrices_Atlanta.png")
-plot_city_gas_prices(master_df, "Atlanta", plot_path)
+
+cities_to_plot = ["Atlanta", "Metro Detroit"]
+output_file = "CountyPrices/GasPrices.png"
+plot_city_gas_prices(master_df, cities_to_plot, output_file)
