53aeae0eb221879af5e077c35c4fdc6dae285c44
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2024-11-23 18:40:54 -0500

    Update realcountymain.py

--- realcountymain.py
+++ realcountymain.py
@@ -5,3 +5,15 @@ from cityscraper import scrape_all_counties
 
 all_gas_prices = scrape_all_counties()
 
+
+# Convert the list of dictionaries to a pandas DataFrame
+df = pd.DataFrame(all_prices)
+
+# Ensure the directory exists
+directory = "/RealCounty"
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+# Save the DataFrame to a CSV file in the specified directory
+csv_file_path = os.path.join(directory, "RealCounty.csv")
+df.to_csv(csv_file_path, index=False)
