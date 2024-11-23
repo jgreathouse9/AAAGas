066d6c1b0dce4ac823ae9a8eb884f40607ccee38
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2024-11-23 18:38:08 -0500

    Update realcountymain.py

--- realcountymain.py
+++ realcountymain.py
@@ -1,7 +1,7 @@
 # THESE ARE THE REAL COUNTY VALUES, the other ones are are the city.
 
-
+from cityscraper import scrape_all_counties
 
 
 all_gas_prices = scrape_all_counties()
-print(all_gas_prices)
+
