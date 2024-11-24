3cce985e4f2c11f39bab43917bbb581feb55f4ab
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2024-11-23 20:05:11 -0500

    Update realcountymain.py

--- realcountymain.py
+++ realcountymain.py
@@ -6,9 +6,10 @@ import os
 all_gas_prices = scrape_all_counties()
 
 # Ensure the directory exists
-directory = "/RealCounty"
+directory = 'RealCounty'  # Relative path, not absolute
 if not os.path.exists(directory):
-    os.makedirs(directory)
+    os.makedirs(directory)  # This will now create the directory in the current working directory
+
 
 # Save the DataFrame to a CSV file in the specified directory
 csv_file_path = os.path.join(directory, "RealCounty.csv")
