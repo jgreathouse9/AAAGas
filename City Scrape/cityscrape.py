1007f9170bdf93ab4380dc238d1c73216b989ee3
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2024-11-30 12:44:14 -0500

    Create cityscrape.py

---
+++ City Scrape/cityscrape.py
@@ -0,0 +1,10 @@
+from cityutils import
+
+# URL of the CSV file
+url = "https://raw.githubusercontent.com/jasonong/List-of-US-States/refs/heads/master/states.csv"
+
+# Read the CSV into a DataFrame
+states_df = pd.read_csv(url)
+
+# Create a dictionary mapping state names to abbreviations
+state_abbreviations = dict(zip(states_df['State'], states_df['Abbreviation']))
