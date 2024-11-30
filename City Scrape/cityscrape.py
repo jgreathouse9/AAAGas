c9b6fd119c28cb14f52eda225b9b6d0406c27ba1
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2024-11-30 12:45:00 -0500

    Update cityscrape.py

--- City Scrape/cityscrape.py
+++ City Scrape/cityscrape.py
@@ -8,3 +8,5 @@ states_df = pd.read_csv(url)
 
 # Create a dictionary mapping state names to abbreviations
 state_abbreviations = dict(zip(states_df['State'], states_df['Abbreviation']))
+
+df = fetch_gas_prices(state_abbreviations)
