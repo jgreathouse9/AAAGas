78925b3a0a49d441a7b4217cec394274ca3b3bd7
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2024-12-01 14:54:48 -0500

    Update county_scraper_main.py

--- RealCounty/county_scraper_main.py
+++ RealCounty/county_scraper_main.py
@@ -5,17 +5,11 @@ from datetime import datetime
 
 today = datetime.now().strftime('%Y-%m-%d')
 
-
-# Fetch gas prices and return as a DataFrame
 df = get_gas_prices(get_state_abbreviations())
 
-# Ensure the directory exists, create it if it doesn't
-directory = './Data'
-
+directory = './RealCounty/Data'
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-# Save the DataFrame to CSV in the specified directory
 filename = f"{directory}/CountyGas{today}.csv"
 df.to_csv(filename, index=False)
-print(f"Saved data to {filename}")
