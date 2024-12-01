0d7c1aa02fb8da27ee543c7d4bc339f6e74d0546
Jared A. Greathouse <82923168+jgreathouse9@users.noreply.github.com>
2024-12-01 14:21:19 -0500

    Update county_scraper_main.py

--- RealCounty/county_scraper_main.py
+++ RealCounty/county_scraper_main.py
@@ -2,4 +2,14 @@ from countyutils import get_state_abbreviations, get_gas_prices
 
 
 # Fetch gas prices and return as a DataFrame
-gas_prices_df = get_gas_prices(get_state_abbreviations())
+df = get_gas_prices(get_state_abbreviations())
+
+# Ensure the directory exists, create it if it doesn't
+directory = './Data'
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+# Save the DataFrame to CSV in the specified directory
+filename = f"{directory}/CountyGas{today}.csv"
+df.to_csv(filename, index=False)
+print(f"Saved data to {filename}")
